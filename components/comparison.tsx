86683865eccb09b95033aa3f2d145e296727efc2
Barrarroso <correodelnino@gmail.com>
2023-10-14 19:47:10 -0400

    comparison icons centered

--- components/comparison.tsx
+++ components/comparison.tsx
@@ -1,55 +1,64 @@
 "use client";
 
-import React, {FC} from "react";
-import { Card, CardBody, CardHeader, Progress, Spacer } from "@nextui-org/react";
-import {CarIcon} from "./CarIcon.jsx";
-import {EnergyIcon} from "./EnergyIcon.jsx";
-import {WasteIcon} from "./WasteIcon.jsx";
-import {OtherIcon} from "./OtherIcon.jsx";
+import React, { FC } from "react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Progress,
+  Spacer,
+} from "@nextui-org/react";
+import { CarIcon } from "./CarIcon.jsx";
+import { EnergyIcon } from "./EnergyIcon.jsx";
+import { WasteIcon } from "./WasteIcon.jsx";
+import { OtherIcon } from "./OtherIcon.jsx";
 import CSS from "csstype";
 
 interface CompProps {
-    val1: number;
-    val2: number;
-    val3: number;
-    val4: number;
-  }
+  val1: number;
+  val2: number;
+  val3: number;
+  val4: number;
+}
 
 const offset: CSS.Properties = {
-    paddingTop: "5px",
-}
+  paddingTop: "5px",
+};
 const margins: CSS.Properties = {
-    marginLeft: "5%",
-    marginRight: "5%",
-  }
+  marginLeft: "5%",
+  marginRight: "5%",
+};
 
-const Comparison: FC<CompProps> = ({val1, val2, val3, val4}) => {
+const Comparison: FC<CompProps> = ({ val1, val2, val3, val4 }) => {
   return (
     <Card
       shadow="sm"
       className="h-full border-none backdrop-blur-[2px] border-neutral-600 bg-background/60 dark:bg-default-700/10"
     >
-        <CardHeader className="flex justify-center text-xl font-bold uppercase">
+      <CardHeader className="flex justify-center text-xl font-bold uppercase">
         <h4>Comparison</h4>
-        </CardHeader>
-      <div style={margins} className="flex flex-col items-center justify-center gap-1">
-        <Spacer/>
-      <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
-      <CarIcon className="text-success" style={offset} />
-      <Progress color="success" aria-label="Loading..." value={val1} />
-      </div>
-      <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
-      <EnergyIcon className="text-success" style={offset} />
-      <Progress color="success" aria-label="Loading..." value={val2} />
-      </div>
-      <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
-      <WasteIcon className="text-success" style={offset} />
-      <Progress color="success" aria-label="Loading..." value={val3} />
-      </div>
-      <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
-      <OtherIcon className="text-success" style={offset} />
-      <Progress color="success" aria-label="Loading..." value={val4} />
-      </div>
+      </CardHeader>
+      <div
+        style={margins}
+        className="flex flex-col items-center justify-center gap-1"
+      >
+        <Spacer />
+        <div className="flex flex-wrap items-center w-full gap-4 mb-6 md:flex-nowrap md:mb-0">
+          <CarIcon className="text-success" style={offset} />
+          <Progress color="success" aria-label="Loading..." value={val1} />
+        </div>
+        <div className="flex flex-wrap items-center w-full gap-4 mb-6 md:flex-nowrap md:mb-0">
+          <EnergyIcon className="text-success" style={offset} />
+          <Progress color="success" aria-label="Loading..." value={val2} />
+        </div>
+        <div className="flex flex-wrap items-center w-full gap-4 mb-6 md:flex-nowrap md:mb-0">
+          <WasteIcon className="text-success" style={offset} />
+          <Progress color="success" aria-label="Loading..." value={val3} />
+        </div>
+        <div className="flex flex-wrap items-center w-full gap-4 mb-6 md:flex-nowrap md:mb-0">
+          <OtherIcon className="text-success" style={offset} />
+          <Progress color="success" aria-label="Loading..." value={val4} />
+        </div>
       </div>
     </Card>
   );
