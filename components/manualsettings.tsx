4c041dcc8eaf59686cc31b0342846ada3cc65b0a
AlexT <alex.thummas66@gmail.com>
2023-10-14 14:14:21 -0400

    Adjusted accordion to be compatible with hooks

--- components/manualsettings.tsx
+++ components/manualsettings.tsx
@@ -1,3 +1,11 @@
+"use client";
+import React from "react";
+import {Accordion, AccordionItem} from "@nextui-org/react";
+import {MonitorMobileIcon} from "./MonitorMobileIcon";
+import Card2 from "@/components/energy-card";
+import Card3 from "@/components/waste-card";
+import Card4 from "@/components/other-card"
+import CSS from 'csstype';
 import { useState } from "react";
 import TransportationForm from "./transportation-card";
 
@@ -15,6 +23,22 @@ export interface TransportationFormProps {
 }
 
 export const ManualSettings = () => {
+  const itemClasses = {
+    base: "py-0 w-full",
+    title: "font-normal text-medium",
+    trigger: "px-2 py-0 data-[hover=true]:bg-default-100 rounded-lg h-14 flex items-center",
+    indicator: "text-medium",
+    content: "text-small px-2",
+  };
+
+  const defaultContent =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+  const innerStyle:CSS.Properties = {
+    overflowY: "scroll", 
+    height: "290px"
+  }
+
   // ==== Transportation form ====
   const [isRegularVehicleMaintenance, setRegularVehicleMaintenance] =
     useState<boolean>(false);
@@ -44,7 +68,27 @@ export const ManualSettings = () => {
 
   return (
     <div>
-      <TransportationForm
+      <Accordion
+      showDivider={false}
+      className="p-2 flex flex-col gap-1 w-full max-w-[450px]"
+      variant="shadow"
+      itemClasses={itemClasses}
+      disableAnimation={true}
+    >
+      <AccordionItem
+        key="1"
+        aria-label="Transportation"
+        startContent={<MonitorMobileIcon className="text-success" />}
+        subtitle={
+          <p className="flex">
+            <em>driving and traveling</em>
+          </p>
+        }
+        title="Transportation"
+      >
+        <div style={innerStyle}>
+        {/* TRANSPORTATION FORM GOES HERE---------------------------------------------------------------*/}
+        <TransportationForm
         isRegularVehicleMaintenance={isRegularVehicleMaintenance}
         setRegularVehicleMaintenance={setRegularVehicleMaintenance}
         milesPerWeekVehicle={milesPerWeekVehicle}
@@ -58,6 +102,67 @@ export const ManualSettings = () => {
         flightsPerYear={flightsPerYear}
         setFlightsPerYear={handleFlightsPerYear}
       />
+        </div>
+      </AccordionItem>
+      <AccordionItem
+        key="2"
+        aria-label="Home Energy"
+        startContent={<MonitorMobileIcon className="text-success" />}
+        subtitle={
+            <p className="flex">
+              <em>energy efficiency and use</em>
+            </p>
+          }
+        title="Home Energy"
+      >
+        <div style={innerStyle}>
+        {/* ENERGY FORM GOES HERE---------------------------------------------------------------*/}
+        <Card2/>
+
+
+        </div>
+      </AccordionItem>
+      <AccordionItem
+        key="3"
+        aria-label="Waste"
+        startContent={<MonitorMobileIcon className="text-success" />}
+        subtitle={
+            <p className="flex">
+              <em>garbage and waste</em>
+            </p>
+          }
+        title="Waste"
+      >
+        <div style={innerStyle}>
+          {/* WASTE FORM GOES HERE---------------------------------------------------------------*/}
+        <Card3/>
+
+
+        </div>
+      </AccordionItem>
+      <AccordionItem
+        key="4"
+        aria-label="Other"
+        startContent={<MonitorMobileIcon className="text-success" />}
+        subtitle={
+            <p className="flex">
+              <em>miscellaneous</em>
+            </p>
+          }
+        title={
+          <p className="flex gap-1 items-center">
+            Other
+          </p>
+        }
+      >
+        <div style={innerStyle}>
+          {/* OTHER FORM GOES HERE---------------------------------------------------------------*/}
+        <Card4/>
+
+
+        </div>
+      </AccordionItem>
+    </Accordion>
     </div>
   );
 };
