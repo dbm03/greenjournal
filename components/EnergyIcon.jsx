fcd322ce76a37c54eb5b2c56e76534685e1677f6
AlexT <alex.thummas66@gmail.com>
2023-10-14 15:14:02 -0400

    Designed Car, Energy, Waste, and Other Icons for accordion

---
+++ components/EnergyIcon.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+export const EnergyIcon = (props) => (
+  <svg
+    aria-hidden="true"
+    fill="none"
+    focusable="false"
+    height="24"
+    role="presentation"
+    viewBox="0 0 24 24"
+    width="24"
+    {...props}
+  >
+    <path
+      d="m6.75.75h7.5l-4.5,9h7.44l-10.44,15,1.5-10.5H.75L6.75.75Z"      
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+    />
+  </svg>
+);
---
+++ components/OtherIcon.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+export const OtherIcon = (props) => (
+  <svg
+    aria-hidden="true"
+    fill="none"
+    focusable="false"
+    height="24"
+    role="presentation"
+    viewBox="0 0 24 24"
+    width="24"
+    {...props}
+  >
+    <path
+      d="m20.75,10.75c0,5.52-4.48,10-10,10S.75,16.27.75,10.75,5.23.75,10.75.75s10,4.48,10,10Zm-10-.65c-.36,0-.65.29-.65.65s.29.65.65.65.65-.29.65-.65-.29-.65-.65-.65Zm-4.54,0c-.36,0-.65.29-.65.65s.29.65.65.65.65-.29.65-.65-.29-.65-.65-.65Zm9.08,0c-.36,0-.65.29-.65.65s.29.65.65.65.65-.29.65-.65-.29-.65-.65-.65Z"      
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+    />
+  </svg>
+);
