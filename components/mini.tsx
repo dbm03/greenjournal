6c7aa8816225fdd0e69aa6eac4a3371295151f2c
AlexT <alex.thummas66@gmail.com>
2023-10-14 20:28:44 -0400

    Added functionality for storing user messages

--- components/mini.tsx
+++ components/mini.tsx
@@ -1,16 +1,26 @@
 import {Card, CardHeader, CardBody, Input, Checkbox, Spacer, Select, SelectItem, Button} from "@nextui-org/react";
 import CSS from "csstype";
 import {FC} from "react";
+import {Popover, PopoverTrigger, PopoverContent} from "@nextui-org/react";
 
   interface MiniProps {
     title: string;
+    content: string;
   }
 
-const Mini: FC<MiniProps> = ({ title }) => {
+const Mini: FC<MiniProps> = ({ title, content }) => {
     return(
-        <Button className="w-min-[130px] h-max-[50px]">
+      <Popover placement="left" showArrow={true}>
+    <PopoverTrigger>
+    <Button className="w-min-[130px] h-max-[50px]">
         {title}
         </Button>
+    </PopoverTrigger>
+    <PopoverContent>
+    {content}
+    </PopoverContent>
+    </Popover>
+
     )
 }
 
--- components/tips.tsx
+++ components/tips.tsx
@@ -1,12 +1,25 @@
 import {Card, CardHeader, CardBody, Input, Checkbox, Spacer, Select, SelectItem, Button} from "@nextui-org/react";
+import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure, Textarea} from "@nextui-org/react";
 import Mini from "@/components/mini";
 import CSS from "csstype";
+import { useState } from "react";
 
 const stretch: CSS.Properties = {
     height: "100%"
 }
+const min: CSS.Properties = {
+    width: "50%",
+    marginBottom: "10px"
+}
 
 const Tips = () => {
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+    const [temp, setTemp] = useState<string>("");
+    const [vals, setVals] = useState<string[]>([]);
+    const log = () => {
+        vals.push(temp);
+        setTemp("");
+      };
     return (
     <Card
       shadow="sm"
@@ -15,14 +28,53 @@ const Tips = () => {
       style={stretch}
     >
                 <CardHeader className="flex justify-center">
-        <h4 className="text-xl font-bold uppercase">History</h4>
+        <h4 className="text-xl font-bold uppercase">Journal</h4>
         </CardHeader>
         <CardBody>
         <div  className="flex flex-col gap-4">
-        <Mini title="10.14.2023" />
-        <Mini title="9.25.2023" />
+        {vals.map((val) =>
+          <Mini title={"Log #" + (String)(vals.indexOf(val)+1)} content={val}></Mini>
+        )}
         </div>
+
         </CardBody>
+        <div className="flex flex-col-reverse justify-center items-center">
+        <Button style={min} color="success" className="text-md" size="sm" onPress={onOpen}><b>+</b></Button>
+        </div>
+
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+            {(onClose) => (
+            <>
+                <ModalHeader className="flex flex-col gap-1">Log Your Progress</ModalHeader>
+                <ModalBody>
+                <p> 
+                    The best way to hold yourself accountable is by logging your progress regularly. Feel free to log what
+                    changes you have made in your life to reduce your carbon footprint or make a commitment to make changes
+                    in the future.
+                </p>
+
+                <Textarea
+      placeholder="An empty space for your thoughts..."
+      className="max-w-md"
+      minRows={8}
+      value={temp}
+      onValueChange={setTemp}
+    />
+
+                </ModalBody>
+                <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                    Close
+                </Button>
+                <Button color="success" onPress={onClose} onPressEnd={log}>
+                    Log
+                </Button>
+                </ModalFooter>
+            </>
+            )}
+        </ModalContent>
+        </Modal>
         </Card>
     )
 }
