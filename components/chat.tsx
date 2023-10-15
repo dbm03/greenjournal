d0ff15a818d0920c9d36d1e821488f5bc220c406
Barrarroso <correodelnino@gmail.com>
2023-10-15 06:24:58 -0400

    Chat without JSOn

--- components/chat.tsx
+++ components/chat.tsx
@@ -6,81 +6,58 @@ import { useContext, useEffect, useState } from "react";
 import { TotalMetricTonsContext } from "./metrictonscontext";
 import { Message } from "ai";
 
-const jsonRegex = /```([\s\S]*?)```([\s\S]*)/;
+// const jsonRegex = /```([\s\S]*?)```([\s\S]*)/;
 
 export default function Chat() {
   const { setTotalMetricTons } = useContext(TotalMetricTonsContext);
 
-  const handleOnFinish = (m: Message) => {
-    if (m.role === "assistant") {
-      if (m.content.startsWith("`")) {
-        const match = jsonRegex.exec(m.content);
-        if (match) {
-          const jsonString = match[1];
-          try {
-            const jsonData = JSON.parse(jsonString);
-            if (
-              jsonData.overall_carbon_score &&
-              typeof jsonData.overall_carbon_score === "number"
-            ) {
-              setTotalMetricTons(
-                (prev) => prev + jsonData.overall_carbon_score
-              );
-            }
-          } catch (error) {
-            console.error("Error parsing JSON");
-          }
-        }
-      }
-    }
-  };
+  // const handleOnFinish = (m: Message) => {
+  //   if (m.role === "assistant") {
+  //     if (m.content.startsWith("`")) {
+  //       const match = jsonRegex.exec(m.content);
+  //       if (match) {
+  //         const jsonString = match[1];
+  //         try {
+  //           const jsonData = JSON.parse(jsonString);
+  //           if (
+  //             jsonData.overall_carbon_score &&
+  //             typeof jsonData.overall_carbon_score === "number"
+  //           ) {
+  //             setTotalMetricTons(
+  //               (prev) => prev + jsonData.overall_carbon_score
+  //             );
+  //           }
+  //         } catch (error) {
+  //           console.error("Error parsing JSON");
+  //         }
+  //       }
+  //     }
+  //   }
+  // };
 
   const { messages, input, handleInputChange, handleSubmit, isLoading } =
     useChat({
       api: "/api/greenai",
-      onFinish: handleOnFinish,
+      // onFinish: handleOnFinish,
     });
-
-  // Example message (needed to parse the JSON separated by triple backticks)
-  /**
-    ```{ "journal_entries": [ { "activity": "Carpooled", "carbon_saved": 0.5 }, { "activity": "Used public transport", "carbon_saved": 0.7 } ], "total_carbon_saved": 1.2 }``` In this week's journal, you carpooled, which saved about 0.5 metric tons of carbon, and you also used public transport, saving approximately 0.7 metric tons of carbon. In total, your carbon-saving efforts for this week amount to 1.2 metric tons. Keep up the great work! Let me know if there's anything else I can assist you with. 
-   */
 
   return (
     <div>
       <div className="px-3">
         <div>
           <span className="text-success">Echo: </span> Hi I am Echo, your
-          personal carbon footprint assistant. I can help you track the change
-          in your carbon footprint and suggest ways to reduce it. Type your
-          message below to get started.
+          personal carbon footprint assistant. How can I help you today?
         </div>
         {messages.map((m) => {
-          if (m.role === "user") {
+          if (m.role === "assistant") {
             return (
               <div key={m.id}>
-                {"User: "}
+                <span className="text-success">Echo: </span>
                 {m.content}
               </div>
             );
           }
-          if (m.role === "assistant") {
-            if (m.content.startsWith("`")) {
-              const match = jsonRegex.exec(m.content);
-              if (match) {
-                const jsonString = match[1];
-                console.log("JSON String:", jsonString);
-                const chatbotOutput = match[2];
-                return (
-                  <div key={m.id}>
-                    <span className="text-success">Echo:</span> {chatbotOutput}
-                  </div>
-                );
-              } else {
-                return;
-              }
-            }
-          }
+          return <div key={m.id}>User: {m.content}</div>;
         })}
       </div>
       <Spacer y={5} />
